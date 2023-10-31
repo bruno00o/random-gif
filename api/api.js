354c3d1c8767834f02b7d070004f86fd3fe2ee6b
bruno00o <bseilliebert@outlook.fr>
2023-10-31 21:30:12 +0100

    little frontend to expose bot's link

--- api/api.js
+++ api/api.js
@@ -18,8 +18,8 @@ const swaggerOptions = {
         },
     },
     apis: [
-        'index.js',
-        './routes/*.js',
+        './api/api.js',
+        './api/routes/*.js',
     ],
 };
 
@@ -32,13 +32,15 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.static('./static/'))
+
 app.use('/random-gif', randomGif);
 
 /* redirects the default url to /docs */
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.redirect('/swagger-random-gif');
 });
-
+ */
 app.use('/swagger-random-gif', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
