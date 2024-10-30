3163e5830241204ea070134147a1108320d1df22
bruno00o <bseilliebert@outlook.fr>
2024-10-30 14:26:19 +0100

    use dilien's api

--- api/routes/randomGif.js
+++ api/routes/randomGif.js
@@ -51,7 +51,8 @@ router.get('/', async (req, res) => {
     if (query.request && query.request != "") {
         word = decodeURIComponent(query.request);
     } else {
-        const response = await fetch('https://random-word.ryanrk.com/api/en/word/random');
+        const response = await fetch('http://tudo7370.odns.fr/v1/word');
+        // const response = await fetch('https://random-word.ryanrk.com/api/en/word/random');
         const data = await response.json();
         word = data[0];
     }
